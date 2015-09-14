96f5c0337bb9879c6ebd8f69707baa34b4851799
David Hayes <davidlynnhayes@att.net>
2015-09-14 18:40:01 -0500

    added a screen shot

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -10,7 +10,7 @@ class ApplicationController < Sinatra::Base
 
   set :views, File.expand_path('../../views',__FILE__)
 
-  set :public_dir, File.expand_path('../../Public', __FILE__)
+  set :public_dir, File.expand_path('../../public', __FILE__)
 
   not_found do
     erb :not_found
